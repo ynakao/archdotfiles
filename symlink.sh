8132486a9d835b3a42b3c2859f06042c03e95151
Yuji Nakao <nakaoyuji.nakao@gmail.com>
2015-06-30 12:54:55 +0900

    Remove .zshrc and add config.fish

--- symlink.sh
+++ symlink.sh
@@ -2,4 +2,4 @@
 ln -sf ~/dotfiles/config ~/.i3/config
 ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/dotfiles/.Xresources ~/.Xresources
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
