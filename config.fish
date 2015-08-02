a5963166addf4fb74b11f75803df02ff025986cd
Yuji Nakao <nakaoyuji.nakao@gmail.com>
2015-08-02 10:20:32 +0900

    Start X at login in fish shell

--- config.fish
+++ config.fish
@@ -1,2 +1,9 @@
+# start X at login
+if status --is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
+        exec startx -- -keeptty
+    end
+end
+
 # Not show initial message
 set -g -x fish_greeting ''
