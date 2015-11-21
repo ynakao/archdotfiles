df0da579420be001e423c1779694348890f3f702
Yuji Nakao <nakaoyuji.nakao@gmail.com>
2015-11-22 06:12:49 +0900

    Fix i3 symlink compatible with XDG

--- symlink.sh
+++ symlink.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ln -sf ~/dotfiles/config ~/.i3/config
+ln -sf ~/dotfiles/config ~/.config/i3/config
 ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/dotfiles/.Xresources ~/.Xresources
 ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
