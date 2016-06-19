112eda8d89dd19a65e4bea5706688a869852fb8c
Yuji Nakao <nakaoyuji.nakao@gmail.com>
2016-06-19 19:45:23 +0900

    Change .nvimrc to init.vim in symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -3,5 +3,5 @@ ln -sf ~/dotfiles/config ~/.config/i3/config
 ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/dotfiles/.Xresources ~/.Xresources
 ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
-ln -sf ~/dotfiles/.nvimrc ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
