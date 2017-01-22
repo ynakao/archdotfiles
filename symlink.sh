0d9a37714aee446259b688aea04bd5ae70c0d2fd
Yuji Nakao <nakaoyuji.nakao@gmail.com>
2017-01-22 22:02:34 +0900

    Add Xmodmap settings; Turn Capslock into Control.

--- symlink.sh
+++ symlink.sh
@@ -2,6 +2,7 @@
 ln -sf ~/dotfiles/config ~/.config/i3/config
 ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/dotfiles/.Xresources ~/.Xresources
+ln -sf ~/dotfiles/.Xmodmap ~/.Xmodmap
 ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
 ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
